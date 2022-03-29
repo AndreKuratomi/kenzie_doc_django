4aa21317793ccb37f0c517fbd41ca2156e115746
KaioIwakiri <kaio_ti@hotmail.com>
2022-03-29 10:05:08 -0300

    feat: appointments views

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -4,8 +4,8 @@ from user.serializers import PatientSerializer, ProfessionalSerializer
 
 class AppointmentsSerializer(serializers.Serializer):
     uuid = serializers.UUIDField(read_only=True)
-    date = serializers.DateTimeField(read_only=True)
+    date = serializers.DateTimeField()
     complaint = serializers.CharField()
     finished = serializers.BooleanField()
-    patient = PatientSerializer(read_only=True)
-    professional = ProfessionalSerializer(read_only=True)
+    patient = PatientSerializer()
+    professional = ProfessionalSerializer()
