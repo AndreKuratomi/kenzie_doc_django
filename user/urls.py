961f8113b75094cba8360a5209e2d973b123d4b6
André Kuratomi <andrekuratomi@gmail.com>
2022-03-28 23:07:20 -0300

    fix: patient update improved but still not entirely working

--- user/urls.py
+++ user/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('patient/', PatientsView.as_view()),
     path('patient/<str:patient_id>/', PatientByIdView.as_view()),
     path('professional/', ProfessionalsView.as_view()),
-    path('professional/<str:council_number>', ProfessionalsByIdView.as_view())
+    path('professional/<str:council_number>/', ProfessionalsByIdView.as_view())
 ]
