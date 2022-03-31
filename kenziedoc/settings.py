24d777629db8e50e4d8a9eca6eaf044bc7e815a4
Pierre Kalil <pierrekalil1@gmail.com>
2022-03-31 17:36:01 -0300

    feat: debug true

--- kenziedoc/settings.py
+++ kenziedoc/settings.py
@@ -91,7 +91,7 @@ if DATABASE_URL:
     db_from_env = dj_database_url.config(
         default=DATABASE_URL, conn_max_age=500, ssl_require=True)
     DATABASES['default'].update(db_from_env)
-    DEBUG = False
+    DEBUG = True
 
 
 # Password validation
