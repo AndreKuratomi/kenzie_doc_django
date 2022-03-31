9036076def300ac7cb81a7b9f3601da150f09e64
Pierre Kalil <pierrekalil1@gmail.com>
2022-03-31 17:23:44 -0300

    feat: add configs in settings database kenziedoc

--- kenziedoc/settings.py
+++ kenziedoc/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -83,6 +85,13 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
+    DEBUG = False
 
 
 # Password validation
